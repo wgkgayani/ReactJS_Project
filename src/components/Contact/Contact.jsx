fc7e787754b1669a7583415ba49df611cb459851
wgkgayani <166277193+wgkgayani@users.noreply.github.com>
2025-08-14 00:17:00 +0530

    Add files via upload
    add contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,7 +2,25 @@ import React from 'react' ;
 import './Contact.css'
 const Contact = () => {
     return (
-        <div>contact</div>
+        <section className='c-wrapper'>
+            <div className='paddings innerWidth flexCenter c-container'>
+                {/*left side*/}
+                <div className='flexColStart c-left'>
+                   <span className='orangeText'>Our Conetacts</span>
+                   <span className='primaryText'>Easy to Contact us</span>
+                   <span className='secondaryText'>We always redy to help by providing the best servise
+                    beleive a good blace to live to live can make your life better
+                   </span>
+                </div>
+
+                {/* right side*/}
+                <div className='c-right'>
+                    <div cl assName='image-container'>
+                        <img src="./contact.jpg" alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
